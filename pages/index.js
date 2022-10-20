6082a1126725d8226e4dd1f0ea30d38e738e6d3d
wuhao <wuhao@otosaas.com>
2022-10-21 00:28:50 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,58 @@ import classNames from 'classnames'
 
 import 'swiper/css'
 
+const data = [
+  {
+    img: '/images/SAF2017_0222576.jpeg',
+    end: 300,
+    duration: 6,
+    text: '航空航天行业参会代表',
+  },
+  {
+    img: '/images/SAF2019_0317645.jpg',
+    end: 180,
+    duration: 6,
+    text: '参会企业',
+    en: 'Companies'
+  },
+  {
+    img: '/images/SAF2016_0195680.jpg',
+    end: 35,
+    duration: 3,
+    text: '智能航电主题演讲',
+    en: 'Keynote Speaker Presentations'
+  },
+  {
+    img: '/images/SAF2017_0238493.jpg',
+    end: 20,
+    duration: 2,
+    text: '航电技术演示',
+    en: 'Technical Presentations'
+  },
+  {
+    img: '/images/SAF2019_0333382.jpg',
+    end: 160,
+    duration: 6,
+    text: '卓越技术高工代表',
+  },
+  {
+    img: '/images/download.jpg',
+    end: 20,
+    duration: 2,
+    text: '行业热点话题',
+  },
+  {
+    img: '/images/Slider1-Turbo-Engine-Solutions.jpg',
+    end: 2,
+    duration: 1,
+    text: '2天航电全产业链盛会',
+  },
+]
+
 export default function Home() {
   const [lenis, setLenis] = useState()
   const [index, setIndex] = useState()
   const [active, setActive] = useState()
-  const [data, setData] = useState({})
 
   const onMouseEnter = (e, index) => {
     e.stopPropagation()
@@ -53,16 +100,6 @@ export default function Home() {
     setActive(-1)
   }
   
-  useEffect(() => {
-    const request = async () => {
-      const baseUrl = 'http://47.92.112.138:3000'
-      const res = await fetch(`${baseUrl}/api/lighlights`)
-      const data = await res.json()
-      setData(data)
-    }
-    request()
-  }, [])
-
   useEffect(() => {
     document.addEventListener('click', reset, false)
     
@@ -148,7 +185,7 @@ export default function Home() {
           <div className="title">会议亮点 HIGHLIGHTS</div>
           <ul className='blinds-list' onMouseLeave={onMouseLeave}>
             {
-              data.data?.map((item, i) => (
+              data.map((item, i) => (
                 <li
                   className={classNames('blinds-item', { 'hover': index === i }, { 'active': active === i })}
                   key={item.text}
